f720e0225f6861f69d11793c6a28e8028117c7d8
Rahul <rahulurmaliya2004@gmail.com>
2025-07-21 09:51:51 +0530

    Initial commit 🚀

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+ 
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [tailwindcss(), autoprefixer()],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5000
+//   }
+// })
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5000
-  }
-})
+  // no need for tailwindcss here!
+});
